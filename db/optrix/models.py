3808b054f8c318e454d4eb14794bf0d13ba68552
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-02-12 16:31:25 -0300

    Added patient and surgery models

--- db/optrix/models.py
+++ db/optrix/models.py
@@ -11,3 +11,19 @@ class Patient(models.Model):
     weight = models.FloatField()
     diagnosis = models.CharField(verbose_name='Disease code (ICD-10)', max_length=16)
     asa_grade = models.FloatField(verbose_name='ASA grade')
+
+class Surgery(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    surgery_type = models.CharField(max_length=256)
+    patient = models.ForeignKey('Patient', on_delete=models.deletion.DO_NOTHING)
+    patient_in = models.TimeField(verbose_name='patient entered the operating room')
+    patient_out = models.TimeField(verbose_name='Patient left the operating room')
+    anesthetic_induction = models.CharField(max_length=512)
+    robotic = models.BooleanField(default=False)
+    incision_time = models.TimeField()
+    suture_time = models.TimeField()
+    estimated_blood_loss = models.FloatField(verbose_name='Blood loss in ml', default=0.0)
+    transfusion = models.BooleanField(default=False)
+    adverse_events = models.TextField(verbose_name="Adverse events during surgery")
+
+
--- db/optrix/rest.py
+++ db/optrix/rest.py
@@ -1,7 +1,11 @@
 from wq.db import rest
-from .models import Patient
+from .models import Patient, Surgery
 
 rest.router.register_model(
     Patient,
+    fields="__all__",
+)
+rest.router.register_model(
+    Surgery,
     fields="__all__",
 )
